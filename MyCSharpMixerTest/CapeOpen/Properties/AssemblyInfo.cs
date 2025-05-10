da23d82c20c95092e31b003e008aad297aa26df5
laugh0608 <laugh608@163.com>
2025-05-10 19:00:03 +0800

    引入发布配置文件

---
+++ MyCSharpMixerTest/CapeOpen/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// 有关组件的一般信息通过以下一组属性进行控制。更改这些属性值以修改与组件关联的信息。
+[assembly: AssemblyTitle("CapeOpen")]
+[assembly: AssemblyDescription("CapeOpen dotNET 翻译类库")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CEPD@BBS")]
+[assembly: AssemblyProduct("CapeOpen")]
+[assembly: AssemblyCopyright("Copyright © EPA 2011-8")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// 将 ComVisible 设置为 false 会使此程序集中的类型对 COM 组件不可见。
+// 如果您需要从 COM 访问此程序集中的类型，请为该类型设置 ComVisible 属性为 true。
+[assembly: ComVisible(true)]
+
+// 以下 GUID 是 typelib 的 ID，如果此项目暴露给 COM。
+[assembly: Guid("d80fc39b-897e-4038-a89e-d89cec9a5fa1")]
+
+// 程序集的版本信息由以下四个值组成：
+// Major Version
+// Minor Version 
+// Build Number
+// Revision
+
+// 您可以指定所有值，或者您可以使用以下所示的“*”作为默认的构建和修订号，例如：[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
