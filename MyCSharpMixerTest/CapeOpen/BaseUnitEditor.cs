c7fd2f4743f090da073417c2802fdee0418835fa
laugh0608 <laugh608@163.com>
2025-05-13 19:42:38 +0800

    BaseUnitEditor

--- MyCSharpMixerTest/CapeOpen/BaseUnitEditor.cs
+++ MyCSharpMixerTest/CapeOpen/BaseUnitEditor.cs
@@ -1,42 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
+﻿// 大白萝卜重构于 2025.05.13，使用 .NET8.O-windows、Microsoft Visual Studio 2022 Preview 和 Rider 2024.3。
 
-namespace CapeOpen
+using System.Runtime.InteropServices;
+
+namespace CapeOpen;
+
+/// <summary>
+/// Base class for a unit operation editor.
+/// </summary>
+/// <remarks> This editor can be used by all unit operations. It creates a tabbed form 
+/// that exposes the unit's properties (parameters and ports) on the first tab. This 
+/// editor can be inherited and tabs added to the tab control to customize the form
+/// for a unit operation.
+/// </remarks>
+[ClassInterface(ClassInterfaceType.None)]
+public partial class BaseUnitEditor : Form
 {
-    /// <summary>
-    /// Base class for a unit operation editor.
-    /// </summary>
-    /// <remarks> This editor can be used by all unit operations. It creates a tabbed form 
-    /// that exposes the unit's properties (parameters and ports) on the first tab. This 
-    /// editor can be inherited and tabs added to the tab control to customize the form
-    /// for a unit operation.
+    private CapeUnitBase _mUnit;
+
+    /// <remarks>
+    /// Constructor for a standard unit operation editor.
     /// </remarks>
-    [System.Runtime.InteropServices.ClassInterface(System.Runtime.InteropServices.ClassInterfaceType.None)]
-    public partial class BaseUnitEditor : Form
+    /// <param name = "unit">The unit operation to be edited.</param>
+    public BaseUnitEditor(CapeUnitBase unit)
     {
-        private CapeUnitBase m_Unit;
+        InitializeComponent();
+        _mUnit = unit;
+        propertyGrid1.SelectedObject = unit;
+    }
 
-        /// <remarks>
-        /// Constructor for a standard unit operation editor.
-        /// </remarks>
-        /// <param name = "unit">The unit operation to be edited.</param>
-        public BaseUnitEditor(CapeUnitBase unit)
-        {
-            InitializeComponent();
-            m_Unit = unit;
-            this.propertyGrid1.SelectedObject = unit;
-        }
-
-        private void CloseButton_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.Close();
-        }
+    private void CloseButton_Click(object sender, EventArgs e)
+    {
+        DialogResult = DialogResult.OK;
+        Close();
     }
 }
